80067ab6b22ae80e40ad304ec91308a8c83c9927
AndreNTeixeira <andrenogueirab@gmail.com>
2025-04-08 21:49:24 -0300

    commit 2

--- src/sistemasdejogo/Videogame.java
+++ src/sistemasdejogo/Videogame.java
@@ -5,7 +5,7 @@ public class Videogame extends Jogo {
     public void salvarPartida(){
         System.out.println("Estou salvando partida de " + getNome());
     }
-    Sca
+
     public Videogame() {
     }
 }
